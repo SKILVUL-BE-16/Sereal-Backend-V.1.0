4a01a63e602289de8fd2ced6a351f357c2c65aa6
yazidr1 <h1101201041@student.untan.ac.id>
2022-11-25 22:28:05 +0700

    test cors

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const db = require('./config/db');
 var cors = require('cors')
 
 const allRoutes = require('./routes');
-const { options } = require('./routes');
 
 const PORT = process.env.PORT || 3000
 
@@ -16,9 +15,9 @@ then(() => {
   console.log(err);
 })
 
-app.use(cors());
 
 app.use(express.json())
+app.use(cors())
 app.use(allRoutes)
 
 app.listen(PORT, () => {
